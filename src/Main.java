611ce428db99418c38e16044a8b5202b46cff4d1
Dael0 <141289093+Dael0@users.noreply.github.com>
2024-07-29 16:42:11 +1000

    Yo what up

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main
+        extends JFrame {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World");
+        Main window = new Main();
+        window.run();
+    }
+
+    class Canvas extends JPanel {
+        public Canvas() {
+            setPreferredSize(new Dimension(720, 720));
+        }
+
+        @Override
+            public void paint(Graphics g) {
+            Point p = getMousePosition();
+            System.out.println(p);
+            Grid grid = new Grid(g, p);
+            }
+    }
+
+    private Main() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas();
+        this.setContentPane(canvas);
+        this.pack();
+        this.setVisible(true);
+    }
+    public void run(){
+        while(true)
+            this.repaint();
     }
 }
