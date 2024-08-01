ec0b439bc89b1bcba7a70b36988e908a3467cea0
jc2026 <james.cheung2@students.mq.edu.au>
2024-08-01 11:36:09 +1000

    Task 3 Complete
    Completed the empty grid from video demonstration for week 2.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame{
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World");
+        Main window = new Main();
+    }
+
+    class Canvas extends JPanel {
+        public Canvas(){
+            setPreferredSize(new Dimension(720,720));
+        }
+    }
+    private Main() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas();
+        this.setContentPane(canvas);
+        this.pack();
+        this.setVisible(b:true);
     }
 }
