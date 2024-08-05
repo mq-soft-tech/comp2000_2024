096acaf9d2e03de0e0736112cb7a0af78736f2cf
AnanyaChugh <126834769+AnanyaChugh@users.noreply.github.com>
2024-08-05 19:44:34 +1000

    task 4 and 5 understanding

--- src/Grid.java
+++ src/Grid.java
@@ -2,22 +2,22 @@ import java.awt.Graphics;
 import java.awt.Point;
 
 public class Grid {
-  // fields
+  // fields-made up of cells.
   Cell[][] cells = new Cell[20][20];
   
   // constructors
-  public Grid() {
+  public Grid() { //default constructor.
     for(int i=0; i<cells.length; i++) {
       for(int j=0; j<cells[i].length; j++) {
-        cells[i][j] = new Cell(10+Cell.size*i, 10+Cell.size*j);
+        cells[i][j] = new Cell(10+Cell.size*i, 10+Cell.size*j);//margin is 10
       }
     }
   }
   // methods
-  public void paint(Graphics g, Point mousePos) {
+  public void paint(Graphics g, Point mousePos) {  //expecting a point as well
     for(int i=0; i<cells.length; i++) {
       for(int j=0; j<cells[i].length; j++) {
-        cells[i][j].paint(g, mousePos);
+        cells[i][j].paint(g, mousePos); //the way the cell draws itself is changed.
       }
     }
   }
