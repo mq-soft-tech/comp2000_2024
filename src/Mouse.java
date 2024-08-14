d17f4828b43b9ebffff19b73a9fd302b8fb361d0
leon8lion <leon.vasington@students.mq.edu.au>
2024-08-14 13:50:15 +1000

    part 2

--- src/Mouse.java
+++ src/Mouse.java
@@ -7,10 +7,8 @@ import javax.swing.JFrame;
 
 
 public class Mouse extends JFrame {
-    int x;
-    int y;
     int size = 10;
-    Point curMousePos = getMousePosition();
+    Point curMousePos = null;
     LinkedList<Point> prevMousePos;
 
     public Mouse(){
@@ -18,24 +16,23 @@ public class Mouse extends JFrame {
     }
     
     public void paint(Graphics g, Point mousePos) {
-      if(mousePos != null){
+      if(mousePos != null && !mousePos.equals(curMousePos)){
+        curMousePos = mousePos;
         prevMousePos.addFirst(new Point(mousePos.x, mousePos.y));
-        if(mousePos != prevMousePos.getFirst()){
-            System.out.println(prevMousePos.getFirst());
-            if (prevMousePos.size() > 100){
-                prevMousePos.removeLast();
-            }
-            for(int i = 0; i<prevMousePos.size();i++){
-                Point pos = prevMousePos.get(i);
-                g.setColor(Color.BLACK);
-                g.fillOval(pos.x, pos.y, size, size);
-                g.setColor(Color.BLACK);
-                g.drawOval(pos.x,pos.y,size,size);
-            } 
+        //System.out.println(mousePos);
+        //System.out.println(prevMousePos.get(0));
+        if (prevMousePos.size() > 100){
+            prevMousePos.removeLast();
         }
 
+        }
+        for(int i = 0; i<prevMousePos.size();i++){
+            Point pos = prevMousePos.get(i);
+            g.setColor(Color.BLACK);
+            g.fillOval(pos.x, pos.y, size, size);
+        } 
+
     }
-    }
+    
 
 }
- //&& mousePos != curMousePos
