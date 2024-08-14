fb3c8c29f4f03b71e228787332f0d1bd4205276e
TanThiNguyen <tanthi.nguyen@students.mq.edu.au>
2024-08-15 09:07:16 +1000

    set color

--- src/mousetrail.java
+++ src/mousetrail.java
@@ -23,7 +23,7 @@ public void add_position(Point p){
 
         for (int i = 0; i < position.size(); i++) {
             Point p = position.get(i);
-            Color circlecolor = Color.GRAY;
+            g.setColor(new Color( 125, 125 ,125, 128) );
             g.fillOval(p.x - 10, p.y - 10, 20, 20); 
             circle -= step;
     }
