eb305ca0e99d029e02a7e7ebdb816f2d6d76feaf
TanThiNguyen <tanthi.nguyen@students.mq.edu.au>
2024-08-14 19:21:48 +1000

    part 2

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,42 @@ import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
 
-public class Main extends JFrame {
+public class Main extends JFrame{
     public static void main(String[] args) throws Exception {
       Main window = new Main();
       window.run();
     }
 
-    class Canvas extends JPanel {
+    class Canvas extends JPanel implements MouseMotionListener {
       Grid grid = new Grid();
       mousetrail trail = new mousetrail();
   
       public Canvas() {
           setPreferredSize(new Dimension(720, 720));
+          addMouseMotionListener(this);
       }
   
       @Override
       public void paint(Graphics g) {
-          Point mousePos = getMousePosition();
-          trail.add_position(mousePos);
-          
-          grid.paint(g, mousePos);
-          trail.paint(g); 
+          super.paint(g);
+          grid.paint(g, getMousePosition());
+          trail.paint(g);
+      }
+
+      @Override
+      public void mouseMoved(MouseEvent e) {
+          trail.add_position(e.getPoint());
+          repaint();
+      }
+
+      @Override
+      public void mouseDragged(MouseEvent e) {
+          trail.add_position(e.getPoint());
+          repaint();
       }
   }
 
