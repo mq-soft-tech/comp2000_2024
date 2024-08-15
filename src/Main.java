68a51e65b96a742ebab7a9ad55498b40c0252600
TanThiNguyen <tanthi.nguyen@students.mq.edu.au>
2024-08-15 10:19:44 +1000

    part 1

--- src/Main.java
+++ src/Main.java
@@ -14,23 +14,17 @@ public class Main extends JFrame {
     class Canvas extends JPanel {
       Grid grid = new Grid();
       mousetrail trail = new mousetrail();
-      Point lastposition = null;
+      
+  
       public Canvas() {
           setPreferredSize(new Dimension(720, 720));
       }
   
       @Override
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            Point currentposition = getMousePosition();
-          if (currentposition != null) {
-            if (lastposition == null || !lastposition.equals(currentposition)) {
-                trail.add_position(currentposition);
-                   lastposition = currentposition;
-     }
-          }
-
-          grid.paint(g, currentposition);
+      public void paint(Graphics g) {
+          Point mousePos = getMousePosition();
+          trail.add_position(mousePos);
+          grid.paint(g, mousePos);
           trail.paint(g); 
       }
   }
