2525705f489d915cf5c399e8bd6795919d2e67b7
Damian Jurd <dnj@drinkinglizard.com>
2024-08-23 19:14:31 +1000

    task 15 solution.

--- src/Grid.java
+++ src/Grid.java
@@ -1,6 +1,7 @@
 import java.awt.Graphics;
 import java.awt.Point;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 public class Grid {
   Cell[][] cells = new Cell[20][20];
@@ -22,11 +23,7 @@ public class Grid {
   }
 
   public void paint(Graphics g, Point mousePos) {
-    for(int i=0; i<cells.length; i++) {
-      for(int j=0; j<cells[i].length; j++) {
-        cells[i][j].paint(g, mousePos);
-      }
-    }
+    doToEachCell( (Cell c) -> c.paint(g, mousePos) );
   }
 
   public Optional<Cell> cellAtColRow(int c, int r) {
@@ -51,4 +48,17 @@ public class Grid {
     }
     return Optional.empty();
   }
+
+    /**
+     * Takes a cell consumer (i.e. a function that has a single `Cell` argument and
+     * returns `void`) and applies that consumer to each cell in the grid.
+     * @param func The `Cell` to `void` function to apply at each spot.
+     */
+    public void doToEachCell(Consumer<Cell> func) {
+      for(int i=0; i < cells.length; i++) {
+        for(int j=0; j < cells[i].length; j++) {
+          func.accept(cells[i][j]);
+        }
+      }
+    }
 }
