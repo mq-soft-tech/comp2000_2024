5950fc1de63984476889f5668a3f81a8e3c65584
Damian Jurd <dnj@drinkinglizard.com>
2024-10-11 15:27:26 +1100

    task 21 solution.

--- src/LeftMostMove.java
+++ src/LeftMostMove.java
@@ -3,13 +3,9 @@ import java.util.List;
 public class LeftMostMove implements MoveStrategy {
   @Override
   public Cell chooseNextLoc(List<Cell> possibleLocs) {
-    Cell currLM = possibleLocs.get(0);
-    for(Cell c: possibleLocs) {
-      if(c.leftOfComparison(currLM) < 0) {
-        currLM = c;
-      }
-    }
-    return currLM;
+    return possibleLocs.stream()
+			.min(Cell::leftOfComparison)
+			.get();
   }
 
   public String toString() {
